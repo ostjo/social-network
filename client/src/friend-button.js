d2929ffc8a06af8d837e8fc47b2d0bf8a00e089d
Johanna Osterrieter <info@johannaost.de>
2021-12-01 14:14:57 +0100

    added functionality

--- client/src/friend-button.js
+++ client/src/friend-button.js
@@ -3,16 +3,16 @@ import { useEffect, useState } from "react";
 export default function FriendButton({ viewed }) {
     const [relState, setRelState] = useState("");
 
-    // LOGIC I:
     // find out our current relationship status with the user we are looking at
     // as a result of this fetch our btn should display the correct txt!
     useEffect(() => {
         fetch(`/api/rel-status/${viewed}`)
             .then((resp) => resp.json())
-            .then((status) => setRelState(status.status));
+            .then((status) => {
+                setRelState(status.status);
+            });
     }, [viewed]);
 
-    //LOGIC II:
     // when the btn gets clicked, we need to update the relationship in the database,
     // and update the btn text to reflect this change
     function updateRelStatus() {
@@ -22,7 +22,11 @@ export default function FriendButton({ viewed }) {
                 "Content-Type": "application/json",
             },
             body: JSON.stringify({ relState: relState, viewed: viewed }),
-        });
+        })
+            .then((resp) => resp.json())
+            .then((status) => {
+                setRelState(status.status);
+            });
     }
 
     return <button onClick={updateRelStatus}>{relState}</button>;
