54f91c350f4633ce709b28c19f3b7f58444a50c9
Johanna Osterrieter <info@johannaost.de>
2021-12-02 16:29:35 +0100

    added styling

--- client/src/registration.js
+++ client/src/registration.js
@@ -9,110 +9,40 @@ export default function Registration() {
 
     return (
         <>
-            <h3>Sign up to become a part of us</h3>
-            {error && <h5 className="error">Oops!</h5>}
-            <form onSubmit={submit}>
-                <input
-                    onChange={handleChange}
-                    name="firstname"
-                    type="text"
-                    placeholder="first name"
-                />
-                <input
-                    onChange={handleChange}
-                    name="lastname"
-                    type="text"
-                    placeholder="last name"
-                />
-                <input
-                    onChange={handleChange}
-                    name="email"
-                    type="email"
-                    placeholder="e-mail"
-                />
-                <input
-                    onChange={handleChange}
-                    name="password"
-                    type="password"
-                    placeholder="password"
-                />
-                <button type="submit">Sign up</button>
-            </form>
-            <p>
-                Already signed up? Log in <Link to="/login">here.</Link>
-            </p>
+            <div className="registration">
+                <h3>Sign up to become a part of us</h3>
+                {error && <h5 className="error">Oops!</h5>}
+                <form onSubmit={submit}>
+                    <input
+                        onChange={handleChange}
+                        name="firstname"
+                        type="text"
+                        placeholder="first name"
+                    />
+                    <input
+                        onChange={handleChange}
+                        name="lastname"
+                        type="text"
+                        placeholder="last name"
+                    />
+                    <input
+                        onChange={handleChange}
+                        name="email"
+                        type="email"
+                        placeholder="e-mail"
+                    />
+                    <input
+                        onChange={handleChange}
+                        name="password"
+                        type="password"
+                        placeholder="password"
+                    />
+                    <button type="submit">Sign up</button>
+                </form>
+                <p>
+                    Already signed up? Log in <Link to="/login">here.</Link>
+                </p>
+            </div>
         </>
     );
 }
-
-// export default class Registration extends Component {
-//     constructor() {
-//         super();
-//         this.state = {};
-//         this.updateInput = this.updateInput.bind(this);
-//         this.register = this.register.bind(this);
-//     }
-//     updateInput({ target }) {
-//         this.setState({
-//             // target.name retrieves the name attribute of the corresponding input field (e.g. name="firstname")
-//             // target.value retrieves the text that was passed into the corresponding input field
-//             [target.name]: target.value,
-//         });
-//     }
-//     register() {
-//         // add the user to our db
-//         fetch("/registration.json", {
-//             method: "POST",
-//             headers: {
-//                 "Content-Type": "application/json",
-//             },
-//             body: JSON.stringify(this.state),
-//         })
-//             .then((resp) => resp.json())
-//             .then((data) => {
-//                 if (data.success) {
-//                     location.replace("/");
-//                 } else {
-//                     this.setState({
-//                         error: true,
-//                     });
-//                 }
-//             });
-//     }
-//     render() {
-//         return (
-//             <>
-//                 <h3>Sign up to become a part of us</h3>
-//                 {this.state.error && <h5 className="error">Oops!</h5>}
-//                 <input
-//                     onChange={this.updateInput}
-//                     name="firstname"
-//                     type="text"
-//                     placeholder="first name"
-//                 />
-//                 <input
-//                     onChange={this.updateInput}
-//                     name="lastname"
-//                     type="text"
-//                     placeholder="last name"
-//                 />
-//                 <input
-//                     onChange={this.updateInput}
-//                     name="email"
-//                     type="email"
-//                     placeholder="e-mail"
-//                 />
-//                 <input
-//                     onChange={this.updateInput}
-//                     name="password"
-//                     type="password"
-//                     placeholder="password"
-//                 />
-//                 <button onClick={this.register}>Sign up</button>
-//                 <p>
-//                     Already signed up? Log in <Link to="/login">here.</Link>
-//                 </p>
-//             </>
-//         );
-//     }
-// }
--- client/src/welcome.js
+++ client/src/welcome.js
@@ -6,19 +6,23 @@ import ResetPassword from "./reset-password";
 export default function Welcome() {
     return (
         <>
-            <img src="../cbc-logo-01.png" className="logo"></img>
-            <h1>Welcome to this Social Network</h1>
-            <BrowserRouter>
-                <Route exact path="/">
-                    <Registration />
-                </Route>
-                <Route path="/login">
-                    <Login />
-                </Route>
-                <Route path="/password">
-                    <ResetPassword />
-                </Route>
-            </BrowserRouter>
+            <div id="start">
+                <div id="intro">
+                    <img src="../cbc-logo-01.png" className="logo"></img>
+                    <h1>Cool Beans only.</h1>
+                </div>
+                <BrowserRouter>
+                    <Route exact path="/">
+                        <Registration />
+                    </Route>
+                    <Route path="/login">
+                        <Login />
+                    </Route>
+                    <Route path="/password">
+                        <ResetPassword />
+                    </Route>
+                </BrowserRouter>
+            </div>
         </>
     );
 }
